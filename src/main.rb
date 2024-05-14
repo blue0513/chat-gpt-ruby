557d8778cce53a9b6a56a7ab50ad965022c25888
Taiju Aoki <taiju.aoki@bizreach.co.jp>
2024-05-14 10:42:03 +0900

    fix: tiktoken returns nil for unknown encoding

--- src/main.rb
+++ src/main.rb
@@ -76,6 +76,8 @@ class Main
   def print_token(model:, content:)
     Prompt.prompt.say("\n")
     enc = Tiktoken.encoding_for_model(model)
+    return unless enc
+
     length = enc.encode(content).length
     Prompt.prompt.ok("token length: #{length}", color: :cyan)
   end
